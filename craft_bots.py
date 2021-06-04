eb753f2a14039734b8a6ea95e9a8481a323adf7b
LNemiro <ludwignemiro@yahoo.com>
2021-06-04 13:22:01 +0100

    Updated default_scenario to create only 3 agents. Redid how the simulation got commands from the agent

--- craft_bots.py
+++ craft_bots.py
@@ -1,4 +1,6 @@
 from world import World
+from agent_api import AgentAPI
+from blank_agent import BlankAgent
 import view
 import threading
 import random as r
@@ -13,25 +15,34 @@ NODE_SIZE = 20
 
 
 def default_scenario(world):
-    for _ in range(5):
+    for _ in range(3):
         world.add_actor(world.nodes[r.randint(0, world.nodes.__len__() - 1)])
         world.add_mine(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 0)
         world.add_mine(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 1)
         world.add_mine(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 2)
         world.add_mine(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 3)
         world.add_mine(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 4)
-        world.add_resource(world.nodes[r.randint(0, world.nodes.__len__() - 1)], 4)
 
 
-def start_simulation(use_gui=True, scenario=default_scenario):
+def start_simulation(agent=None, use_gui=True, scenario=default_scenario):
     world = World(green_decay_time=120)
     scenario(world)
+    if agent is not None:
+        agent.api = AgentAPI(world)
+    else:
+        agent = BlankAgent()
+        agent.api = AgentAPI(world)
     if use_gui:
-        gui = init_gui(world, call_repeatedly(1 / TICK_HZ, refresh_world, world))
+        stop_sim_ticking = call_repeatedly(1 / TICK_HZ, refresh_world, world, agent)
+
+        def sim_stop():
+            stop_sim_ticking()
+            agent.stop = True
+        gui = init_gui(world, sim_stop)
         refresh_gui(gui)
         gui.mainloop()
     else:
-        call_repeatedly(1 / TICK_HZ, refresh_world, world)
+        call_repeatedly(1 / TICK_HZ, refresh_world, world, agent)
 
 
 def refresh_gui(gui):
@@ -43,8 +54,15 @@ def refresh_gui(gui):
     gui.after(math.ceil(1000 / TICK_HZ), refresh_gui_wrapper)
 
 
-def refresh_world(world):
+def refresh_world(world, agent):
+    if not world.command_queue and not agent.thinking:
+        agent.thinking = True
+        agent_thread = threading.Thread(target=agent.get_next_commands)
+        agent_thread.start()
     world.run_tick()
+    if world.command_results:
+        agent.receive_results(world.command_results)
+    agent.api.num_of_current_commands = 0
 
 
 def call_repeatedly(interval, func, *args):
