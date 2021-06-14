cd2d77097c2e971a1c459af2088ccff6d54b66e1
LNemiro <ludwignemiro@yahoo.com>
2021-06-14 13:19:01 +0100

    Updated blank agent to meet required specs of an Agent

--- blank_agent.py
+++ blank_agent.py
@@ -5,9 +5,10 @@ class BlankAgent:
     def __init__(self):
         self.api = None
         self.thinking = False
+        self.world_info = None
 
     def receive_results(self, _):
         self.thinking = False
 
     def get_next_commands(self):
-        time.sleep(1)
+        self.api.no_commands()
