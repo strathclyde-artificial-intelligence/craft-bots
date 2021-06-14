815469797909e3182ac47e46bede2c0a49a9e499
LNemiro <ludwignemiro@yahoo.com>
2021-06-14 13:18:32 +0100

    Removed redundant commands now that information is gathered through the dictionary system

--- agent_api.py
+++ agent_api.py
@@ -5,7 +5,7 @@ class AgentAPI:
 
     def __init__(self, world, max_commands=10):
         self.world = world
-        self.max_commands = 3
+        self.max_commands = max_commands
         self.num_of_current_commands = 0
 
     def send_command(self, function_id, *args):
@@ -16,73 +16,34 @@ class AgentAPI:
         return -1
 
     def move_to(self, actor_id, node_id):
-        return self.send_command(0, actor_id, node_id)
+        return self.send_command(Command.MOVE_TO, actor_id, node_id)
 
     def move_rand(self, actor_id):
-        return self.send_command(1, actor_id)
+        return self.send_command(Command.MOVE_RAND, actor_id)
 
     def pick_up_resource(self, actor_id, resource_id):
-        return self.send_command(2, actor_id, resource_id)
+        return self.send_command(Command.PICK_UP_RESOURCE, actor_id, resource_id)
 
     def drop_resource(self, actor_id, resource_id):
-        return self.send_command(3, actor_id, resource_id)
+        return self.send_command(Command.DROP_RESOURCE, actor_id, resource_id)
 
     def drop_all_resources(self, actor_id):
-        return self.send_command(4, actor_id)
+        return self.send_command(Command.DROP_ALL_RESOURCES, actor_id)
 
-    def mine_at(self, actor_id, mine_id):
-        return self.send_command(5, actor_id, mine_id)
+    def dig_at(self, actor_id, mine_id):
+        return self.send_command(Command.DIG_AT, actor_id, mine_id)
 
     def start_site(self, actor_id, site_type):
-        return self.send_command(6, actor_id, site_type)
+        return self.send_command(Command.START_SITE, actor_id, site_type)
 
     def build_at(self, actor_id, site_id):
-        return self.send_command(7, actor_id, site_id)
+        return self.send_command(Command.BUILD_AT, actor_id, site_id)
 
     def deposit_resources(self, actor_id, site_id, resource_id):
-        return self.send_command(8, actor_id, site_id, resource_id)
-
-    def get_adjacent_nodes(self, node_id):
-        return self.send_command(9, node_id)
-
-    def get_actors_at(self, node_id):
-        return self.send_command(10, node_id)
-
-    def get_all_actors(self):
-        return self.send_command(11)
-
-    def get_resources_at(self, node_id):
-        return self.send_command(12, node_id)
-
-    def get_all_mines(self):
-        return self.send_command(13)
-
-    def get_mines_at(self, node_id):
-        return self.send_command(14, node_id)
-
-    def get_all_resources(self):
-        return self.send_command(15)
-
-    def get_sites_at(self, node_id):
-        return self.send_command(16, node_id)
-
-    def get_all_sites(self):
-        return self.send_command(17)
-
-    def get_buildings_at(self, node_id):
-        return self.send_command(18, node_id)
-
-    def get_all_buildings(self):
-        return self.send_command(19)
-
-    def get_tasks(self):
-        return self.send_command(20)
-
-    def get_all_nodes(self):
-        return self.send_command(21)
+        return self.send_command(Command.DEPOSIT_RESOURCES, actor_id, site_id, resource_id)
 
     def no_commands(self):
-        return self.send_command(22)
+        return self.send_command(Command.NO_COMMAND)
 
     def cancel_action(self, actor_id):
-        return self.send_command(23, actor_id)
+        return self.send_command(Command.CANCEL_ACTION, actor_id)
--- command.py
+++ command.py
@@ -1,4 +1,17 @@
 class Command:
+
+    MOVE_TO = 0
+    MOVE_RAND = 1
+    PICK_UP_RESOURCE = 2
+    DROP_RESOURCE = 3
+    DROP_ALL_RESOURCES = 4
+    DIG_AT = 5
+    START_SITE = 6
+    BUILD_AT = 7
+    DEPOSIT_RESOURCES = 8
+    NO_COMMAND = 9
+    CANCEL_ACTION = 10
+
     def __init__(self, world, function_id, *args):
         self.world = world
         self.id = self.world.get_new_id()
@@ -7,76 +20,43 @@ class Command:
         self.args = args
 
     def perform(self):
-        if self.function_id == 0 and self.args.__len__() == 2:
+        if self.function_id == Command.MOVE_TO and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             target_node = self.world.get_by_id(self.args[1], entity_type="Node")
             return actor.travel_to(target_node)
-        elif self.function_id == 1 and self.args.__len__() == 1:
+        elif self.function_id == Command.MOVE_RAND and self.args.__len__() == 1:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             return actor.travel_rand()
-        elif self.function_id == 2 and self.args.__len__() == 2:
+        elif self.function_id == Command.PICK_UP_RESOURCE and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             resource = self.world.get_by_id(self.args[1], entity_type="Resource")
             return actor.pick_up_resource(resource)
-        elif self.function_id == 3 and self.args.__len__() == 2:
+        elif self.function_id == Command.DROP_RESOURCE and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             resource = self.world.get_by_id(self.args[1], entity_type="Resource")
             return actor.drop_resource(resource)
-        elif self.function_id == 4 and self.args.__len__() == 1:
+        elif self.function_id == Command.DROP_ALL_RESOURCES and self.args.__len__() == 1:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             return actor.drop_everything()
-        elif self.function_id == 5 and self.args.__len__() == 2:
+        elif self.function_id == Command.DIG_AT and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             mine = self.world.get_by_id(self.args[1], entity_type="Mine")
-            return actor.mine_at(mine)
-        elif self.function_id == 6 and self.args.__len__() == 2:
+            return actor.dig_at(mine)
+        elif self.function_id == Command.START_SITE and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             return actor.start_site(self.args[1])
-        elif self.function_id == 7 and self.args.__len__() == 2:
+        elif self.function_id == Command.BUILD_AT and self.args.__len__() == 2:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             site = self.world.get_by_id(self.args[1], entity_type="Site")
             return actor.build_at(site)
-        elif self.function_id == 8 and self.args.__len__() == 3:
+        elif self.function_id == Command.DEPOSIT_RESOURCES and self.args.__len__() == 3:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             site = self.world.get_by_id(self.args[1], entity_type="Site")
             resource = self.world.get_by_id(self.args[2], entity_type="Resource")
-            print(resource)
             return actor.deposit(site, resource)
-        elif self.function_id == 9 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.get_adjacent_nodes()
-        elif self.function_id == 10 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.actors
-        elif self.function_id == 11 and self.args.__len__() == 0:
-            return self.world.get_all_actors()
-        elif self.function_id == 12 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.resources
-        elif self.function_id == 13 and self.args.__len__() == 0:
-            return self.world.get_all_mines()
-        elif self.function_id == 14 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.mines
-        elif self.function_id == 15 and self.args.__len__() == 0:
-            return self.world.get_all_resources()
-        elif self.function_id == 16 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.sites
-        elif self.function_id == 17 and self.args.__len__() == 0:
-            return self.world.get_all_sites()
-        elif self.function_id == 18 and self.args.__len__() == 1:
-            node = self.world.get_by_id(self.args[0], entity_type="Node")
-            return node.buildings
-        elif self.function_id == 19 and self.args.__len__() == 0:
-            return self.world.get_all_buildings()
-        elif self.function_id == 20 and self.args.__len__() == 0:
-            return self.world.tasks
-        elif self.function_id == 21 and self.args.__len__() == 0:
-            return self.world.nodes
-        elif self.function_id == 22 and self.args.__len__() == 0:
+        elif self.function_id == Command.NO_COMMAND and self.args.__len__() == 0:
             return None
-        elif self.function_id == 23 and self.args.__len__() == 1:
+        elif self.function_id == Command.CANCEL_ACTION and self.args.__len__() == 1:
             actor = self.world.get_by_id(self.args[0], entity_type="Actor")
             return actor.cancel_action()
         else:
