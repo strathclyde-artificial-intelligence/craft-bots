20dfb39ee56509ff891646074855ac13ab9c600d
LNemiro <ludwignemiro@yahoo.com>
2021-06-28 15:02:28 +0100

    Removed all remnants of previous task system

--- entities/task.py
+++ entities/task.py
@@ -1,4 +1,6 @@
 import random as r
+from entities.building import Building
+
 
 class Task:
 
@@ -6,52 +8,38 @@ class Task:
     MEDIUM = 1
     HARD = 2
 
-    def __init__(self, world, node, colour, amount):
+    def __init__(self, world):
         """
         A task in the craftbots simulation. Tasks are randomly generated goals used to give agents something to aim
-        towards in the simulation. They ask the agent to create a certain number of buildings of a certain colour.
+        towards in the simulation. They ask the agent to create a purple building (a goal building) that has randomly
+        generated resource requirements.
         :param world: the world which the task is associated with
-        :param node: the node which the buildings must be completed at
-        :param colour: the colour of buildings to be built
-        :param amount: the number of buildings to be built
         """
         self.node = world.nodes[r.randint(0, world.nodes.__len__() - 1)]
-        self.colour = colour
-        self.amount = amount
         self.world = world
         self.id = self.world.get_new_id()
+        self.difficulty = self.__decide_difficulty()
         self.needed_resources = self.__generate_task()
         self.project = None
 
-        self.fields = {"node": self.node.id, "colour": colour, "amount": amount, "id": self.id,
-                       "completed": self.completed, "needed_resources": self.needed_resources, "project": self.project}
+        self.fields = {"node": self.node.id, "id": self.id, "completed": self.completed, "difficulty": self.difficulty,
+                       "needed_resources": self.needed_resources, "project": self.project}
 
     def __repr__(self):
-        return "Task(" + str(self.amount) + ", " + self.world.get_colour_string(self.colour) +\
-               ", " + str(self.node) + ")"
+        return "Task(" + str(self.node) + ", " + str(self.needed_resources) + ")"
 
     def __str__(self):
-        if self.amount == 1:
-            return "Task to build 1 " + self.world.get_colour_string(self.colour) + " building at " + str(self.node)
-        return "Task to build " + str(self.amount) + " " + self.world.get_colour_string(self.colour) \
-               + " buildings at " + str(self.node)
+        return "Task to build purple building at " + str(self.node) + " with the resource requirements of " + \
+               str(self.needed_resources)
 
     def completed(self):
         """
-        Checks if the minimum amount of buildings of the right colour have been built and return True or False if that
-        has been fulfilled yet or not
+        Checks if the tasks project is a Building entity. If it is then it implies that the building is complete and as
+        such the task is complete. If the project is None or a Site entity, then it implies that there is still work to
+        do before the task is complete.
         :return: True if the task is completed, and False if not
         """
-        current_amount = 0
-        for building in self.node.buildings:
-            if building.colour == self.colour:
-                current_amount += 1
-                if current_amount >= self.amount:
-                    self.fields.__setitem__("complete", True)
-                    return True
-        return False
-
-    def complete(self):
+        return isinstance(self.project, Building)
 
     def __generate_task(self):
         """
@@ -60,11 +48,10 @@ class Task:
 
         :return: A list of needed resources for a Site entity to use
         """
-        difficulty = self.__get_difficulty()
-        num_of_types = self.__get_num_of_types(difficulty)
-        return self.__get_num_of_resources(difficulty, num_of_types)
 
-    def __get_difficulty(self):
+        return self.__get_num_of_resources(self.__get_num_of_types())
+
+    def __decide_difficulty(self):
         """
         Returns the difficulty of the task. This value is determined randomly based on parameters from the
         initialisation files under modifiers.
@@ -80,28 +67,26 @@ class Task:
         else:
             return Task.HARD
         
-    def __get_num_of_types(self, difficulty):
+    def __get_num_of_types(self):
         """
         Returns the number of different resource types to be used in the Task based on the difficulty of the task. This
         value is determined randomly based on parameters from the initialisation files under modifiers.
 
-        :param difficulty: The difficulty of the Task
         :return: The number of different resource types to be used in the Task
         """
-        if difficulty == Task.EASY:
+        if self.difficulty == Task.EASY:
             return r.randint(self.world.modifiers["EASY_TASK_MIN_TYPES"], self.world.modifiers["EASY_TASK_MAX_TYPES"])
-        elif difficulty == Task.MEDIUM:
+        elif self.difficulty == Task.MEDIUM:
             return r.randint(self.world.modifiers["MEDIUM_TASK_MIN_TYPES"], 
                              self.world.modifiers["MEDIUM_TASK_MAX_TYPES"])
         else:
             return r.randint(self.world.modifiers["HARD_TASK_MIN_TYPES"], self.world.modifiers["HARD_TASK_MAX_TYPES"])
 
-    def __get_num_of_resources(self, difficulty, num_of_types):
+    def __get_num_of_resources(self, num_of_types):
         """
         Returns a set of needed resources based on the difficulty of the task and the number of resource types to be
         used. These values are determined randomly based on parameters from the initialisation files under modifiers.
 
-        :param difficulty: The difficulty of the Task
         :param num_of_types: The number of different resource types to be used in the Task
         :return: A list of needed resources for a Site entity to use
         """
@@ -115,10 +100,10 @@ class Task:
         needed_resources = [0, 0, 0, 0, 0]
         for index in range(needed_resources.__len__()):
             if chosen.__contains__(index):
-                if difficulty == Task.EASY:
+                if self.difficulty == Task.EASY:
                     min_res = self.world.modifiers["EASY_TASK_MIN_RESOURCES"]
                     max_res = self.world.modifiers["EASY_TASK_MAX_RESOURCES"]
-                elif difficulty == Task.MEDIUM:
+                elif self.difficulty == Task.MEDIUM:
                     min_res = self.world.modifiers["MEDIUM_TASK_MIN_RESOURCES"]
                     max_res = self.world.modifiers["MEDIUM_TASK_MAX_RESOURCES"]
                 else:
