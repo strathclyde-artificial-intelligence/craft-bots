1968d0186e22ac4408f5411bf046268f397cea9e
LNemiro <ludwignemiro@yahoo.com>
2021-07-04 16:33:34 +0100

    Main now runs simulation with TestAgent

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from agents import test_agent
 
 if __name__ == '__main__':
     # craft_bots.start_simulation(agent=bogo.Bogo())
-    print(craft_bots.start_simulation(agent=blank_agent.BlankAgent()))
-    # craft_bots.start_simulation(agent=test_agent.TestAgent())
+    # craft_bots.start_simulation(agent=blank_agent.BlankAgent())
+    craft_bots.start_simulation(agent=test_agent.TestAgent())
