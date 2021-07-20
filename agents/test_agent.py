680228176f6f1e15134c6eda82c6a42f14855541
LNemiro <ludwignemiro@yahoo.com>
2021-07-20 05:49:58 +0100

    Test agent sends actors around randomly until two actors meet, at which point the one with the highest ID will constantly say hello friend, and all other actors listen to the message

--- agents/test_agent.py
+++ agents/test_agent.py
@@ -8,11 +8,25 @@ class TestAgent:
         self.world_info = None
 
     def receive_results(self, results):
-        print(results)
+        pass
+        # print(results)
 
     def get_next_commands(self):
         self.api: agent_api.AgentAPI
-        for actor_id in self.world_info["actors"]:
+        for actor_id in self.api.actors:
             if self.api.get_field(actor_id, "state") == 0:
+                if self.world_info["actors"].__len__() > 1:
+                    for current_actor_id in self.world_info["actors"]:
+                        if current_actor_id != actor_id:
+                            self.api.start_sending(actor_id, "hello friend!")
                 self.api.move_rand(actor_id)
+            if self.api.get_field(actor_id, "state") == 6:
+                for current_actor_id in self.world_info["actors"]:
+                    if current_actor_id > actor_id:
+                        self.api.cancel_action(actor_id)
+                        self.api.start_receiving(actor_id)
+
+        if self.world_info["tick"] % 10 == 0:
+            for actor_id in self.world_info["actors"]:
+                print(self.api.get_field(actor_id, "target"))
         self.thinking = False
