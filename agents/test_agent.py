076d1ad6f9db3f34144af8a1f4e39936948c84f7
LNemiro <ludwignemiro@yahoo.com>
2021-07-26 12:59:34 +0100

    Changed Test Agent to copy Wiki

--- agents/test_agent.py
+++ agents/test_agent.py
@@ -1,4 +1,6 @@
 from api import agent_api
+from api.command import Command
+from entities.actor import Actor
 
 
 class TestAgent:
@@ -6,27 +8,16 @@ class TestAgent:
         self.api = None
         self.thinking = False
         self.world_info = None
-
-    def receive_results(self, results):
-        pass
-        # print(results)
+        self.pending = []
 
     def get_next_commands(self):
         self.api: agent_api.AgentAPI
         for actor_id in self.api.actors:
             if self.api.get_field(actor_id, "state") == 0:
-                if self.world_info["actors"].__len__() > 1:
-                    for current_actor_id in self.world_info["actors"]:
-                        if current_actor_id != actor_id:
-                            self.api.start_sending(actor_id, "hello friend!")
-                self.api.move_rand(actor_id)
-            if self.api.get_field(actor_id, "state") == 6:
-                for current_actor_id in self.world_info["actors"]:
-                    if current_actor_id > actor_id:
-                        self.api.cancel_action(actor_id)
-                        self.api.start_receiving(actor_id)
-
-        if self.world_info["tick"] % 10 == 0:
-            for actor_id in self.world_info["actors"]:
-                print(self.api.get_field(actor_id, "target"))
+                self.pending.append(self.api.move_rand(actor_id))
         self.thinking = False
+
+        for command_id in self.pending:
+            if self.api.get_field(command_id, "state") == Command.COMPLETED:
+                print(self.api.get_field(command_id, "result"))
+                self.pending.remove(command_id)
