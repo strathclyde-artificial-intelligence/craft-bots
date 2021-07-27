4cbfe130129a0660c5025ad0035cef3928e8f883
LNemiro <ludwignemiro@yahoo.com>
2021-07-27 12:36:55 +0100

    Main now runs TestAgent by default

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from craftbots import craft_bots
-from agents import bogo
-from agents import blank_agent
 from agents import test_agent
-from agents import human_agent
+from craftbots import craft_bots
 
 if __name__ == '__main__':
-    craft_bots.start_simulation(agent_class=bogo.Bogo)
-    # craft_bots.start_simulation(agent_class=blank_agent.BlankAgent)
-    # craft_bots.start_simulation(agent_class=test_agent.TestAgent)
-    # craft_bots.start_simulation(agent_class=human_agent.HumanAgent)
+    craft_bots.start_simulation(agent_class=test_agent.TestAgent)
