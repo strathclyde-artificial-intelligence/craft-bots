c4381d6a21343f791a2dd25914743cba61597ebe
Michael Cashmore <michael.cashmore@kcl.ac.uk>
2022-06-14 22:23:40 +0100

    Added argparser to main so that config can be easily selected

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import argparse
 from agents.rule_based_agent import RBAgent
 from craftbots.simulation import Simulation
 from gui.main_window import CraftBotsGUI
 
 if __name__ == '__main__':
 
+    # parse command line arguments
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-f', help="configuration file", type=str, default='craftbots/config/simple_configuration.yaml')
+    args = arg_parser.parse_args()
+
     # Simulation
-    sim = Simulation(configuration_file='craftbots/config/simple_configuration.yaml')
+    sim = Simulation(configuration_file=args.f)
 
     # agent
     agent = RBAgent()
